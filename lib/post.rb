0f2d97e2e3c3eb199df59f29b25ece9b02e7e3a0
James Harris <mharris9608@yahoo.com>
2021-03-01 19:52:19 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,26 @@
+class Post
+
+  @@all = []
+  
+  def initalize(title)
+    @title = title
+    @@all << @title
+  end
+
+  def all
+
+  end
+
+  def title
+
+  end
+
+  def author
+
+  end
+
+  def author_name
+
+  end
+
+end
