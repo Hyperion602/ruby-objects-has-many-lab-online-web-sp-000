955ec38f90dfc4fc3e4a38bd18d20609cc283bd3
James Harris <mharris9608@yahoo.com>
2021-03-01 23:06:37 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,18 +6,20 @@ class Author
   end
 
   def posts
-
+    Post.all.select do |post|
+      post.author == self
+    end
   end
 
-  def add_post
-
+  def add_post(post)
+    post.author = self
   end
 
-  def add_post_by_title
-
+  def add_post_by_title(title)
+    Post.new(title).author = self
   end
 
-  def post_count
-
+  def self.post_count
+    Post.all.length
   end
 end
